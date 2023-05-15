0a80610e5c7535b7fe50b71e454da8721375e773
Will <williamvdm@live.nl>
2023-05-15 10:17:00 +0200

    add: login input & buttons

--- src/main/java/com/example/aiassistent/UserController.java
+++ src/main/java/com/example/aiassistent/UserController.java
@@ -3,8 +3,13 @@ package com.example.aiassistent;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class UserController {
+    public Button loginbutton;
+    public TextField usernamefield;
+    public TextField passwordfield;
+
     @FXML
     private void loginButtonEvent(ActionEvent event) {
         AssistentApplication.showChatScene();

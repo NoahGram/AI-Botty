08e075bbaf4b65361e01672f4764b360f77f63cb
Noah Gram <noah@gramonline.nl>
2023-05-15 11:30:57 +0200

    add Schufft login

---
+++ src/main/java/com/example/aiassistent/LoginPageController.java
@@ -0,0 +1,51 @@
+package com.example.aiassistent;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
+
+public class LoginPageController {
+    public Button loginbutton;
+    public TextField usernamefield;
+    public TextField passwordfield;
+    public TextField errorField;
+
+    private HashMap<String, String> userMap = new HashMap<>();
+
+    public LoginPageController() {
+        // Initialize the userMap with some sample data
+        userMap.put("alice", "password1");
+        userMap.put("bob", "password2");
+        userMap.put("charlie", "password3");
+    }
+
+    @FXML
+    private void loginButtonEvent(ActionEvent event) {
+        String username = usernamefield.getText();
+        String password = passwordfield.getText();
+        if (userMap.containsKey(username) && userMap.get(username).equals(password)) {
+            AssistentApplication.showChatScene();
+        } else {
+            errorField.setText("Invalid username or password.");
+        }
+    }
+
+    @FXML
+    private void registerButtonEvent(ActionEvent event) {
+        String username = usernamefield.getText();
+        String password = passwordfield.getText();
+        if (userMap.containsKey(username)) {
+            errorField.setText("Username already taken.");
+        } else {
+            userMap.put(username, password);
+            AssistentApplication.showChatScene();
+        }
+    }
+
+}
+
+
--- src/main/java/com/example/aiassistent/MainController.java
+++ src/main/java/com/example/aiassistent/MainController.java
@@ -1,10 +1,7 @@
 package com.example.aiassistent;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.text.Text;
-
 public class MainController {
     public Text logo;
+
 
 }

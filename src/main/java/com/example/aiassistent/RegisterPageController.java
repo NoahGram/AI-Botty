960b8a8db812c4c871f640ca2e3a9176ba6469a7
j9mmy <jimmy.tukiran@outlook.com>
2023-05-23 22:46:15 +0200

    add: input validation, change theme functionality etc.

--- src/main/java/com/example/aiassistent/RegisterPageController.java
+++ src/main/java/com/example/aiassistent/RegisterPageController.java
@@ -4,23 +4,32 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
 import java.util.HashMap;
 
 public class RegisterPageController {
-    public Button loginbutton;
-    public TextField usernamefield;
-    public TextField passwordfield;
-
+    @FXML public GridPane background;
+    @FXML public TextField emailField;
+    @FXML public TextField usernamefield;
+    @FXML public TextField passwordfield;
+    @FXML public Text invalid;
+    public boolean isLightMode = false;
     UserAccountSingleton userAccounts = UserAccountSingleton.getInstance();
 
+    @FXML
     public void registerButtonEvent(ActionEvent event) {
         String username = usernamefield.getText();
         String password = passwordfield.getText();
+        String email = emailField.getText();
         if (userAccounts.UserExists(username)) {
-            //errorField.setText("Username already taken.");
+            invalid.setText("Gebruikersnaam is al genomen");
+        } else if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+            invalid.setText("Voer alstublieft alle velden in.");
         } else {
-            userAccounts.AddUser(new User(username, password, ""));
+            userAccounts.AddUser(new User(username, password, email));
             AssistentApplication.showChatScene();
         }
     }
@@ -28,5 +37,35 @@ public class RegisterPageController {
     @FXML
     private void loginPage(ActionEvent event) {
         AssistentApplication.showLoginScene();
+        invalid.setText("");
+    }
+
+    @FXML
+    private void onEnter(ActionEvent event) {
+        registerButtonEvent(event);
+    }
+
+    @FXML
+    private void settingPage(ActionEvent event) {
+        AssistentApplication.showSettingScene();
+        invalid.setText("");
+    }
+
+    @FXML
+    private void changeTheme(ActionEvent event) {
+        if (isLightMode) {
+            background.getStylesheets().remove(getClass().getResource("registerLight.css").toExternalForm());
+            background.getStylesheets().add(getClass().getResource("registerDark.css").toExternalForm());
+            isLightMode = false;
+        } else {
+            background.getStylesheets().remove(getClass().getResource("registerDark.css").toExternalForm());
+            background.getStylesheets().add(getClass().getResource("registerLight.css").toExternalForm());
+            isLightMode = true;
+        }
+    }
+
+    @FXML
+    private void exit(ActionEvent event) {
+        System.exit(0);
     }
 }
